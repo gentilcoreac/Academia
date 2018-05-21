cb5a0ea4573ca4b8bece2c908da2f00518fa5473
bdolce <37807863+bdolce@users.noreply.github.com>
2018-05-21 13:26:06 -0300

    Lab04 - Añadido grilla para presentacion de datos en Proyecto Escritorio

---
+++ UI.Desktop/Usuarios.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripContainer1_ContentPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvUsuarios.AutoGenerateColumns = false;
+            this.dgvUsuarios.DataSource = ul.GetAll();
+            
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dgvUsuarios_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tsbEditar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
