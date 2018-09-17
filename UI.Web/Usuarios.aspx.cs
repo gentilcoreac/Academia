7e33130015e5ec023f17e0d186b85766b9705e53
bdolce <brunodolce96@gmail.com>
2018-09-17 19:45:15 -0300

    Añadido ABM Usuarios vista Web

---
+++ UI.Web/Usuarios.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UI.Web
+{
+    public partial class Usuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void grdUsuarios_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.ToLower() == "insertar")
+            {
+                TextBox cajaTexto;
+                CheckBox cbHabil;
+                string textoActual;
+                Business.Entities.Usuario usuarioNuevo = new Business.Entities.Usuario();
+
+                //Busco el control y lo asigno a la propiedad correspondiente del objeto Usuario
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtNombre");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Nombre = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtApellido");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Apellido = textoActual;
+                
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtEmail");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Email = textoActual;
+
+                cbHabil = (CheckBox)grdUsuarios.FooterRow.FindControl("cbHabilitado");
+                usuarioNuevo.Habilitado = cbHabil.Checked;
+
+                Business.Logic.UsuarioLogic usulogic = new Business.Logic.UsuarioLogic();
+                usulogic.Save(usuarioNuevo);
+                grdUsuarios.DataBind();
+
+            }
+        }
+    }
+}
