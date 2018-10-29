6d4bb5b586c6c2cf2b4ae503137a61c999ab7945
bdolce <37807863+bdolce@users.noreply.github.com>
2018-10-29 12:42:10 -0300

    Agregado gestión de personal (OJO; todavia no esta del todo funcional)

---
+++ Web/Personas.aspx
@@ -0,0 +1,138 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Personas.aspx.cs" Inherits="Web.Personas" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+	<style type="text/css">
+		.auto-style1 {
+			width: 128px;
+		}
+	</style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:GridView ID="grdPersonas" runat="server" DataSourceID="odsPersonas" OnRowCommand="grdPersonas_RowCommand" ShowFooter="True" OnSelectedIndexChanged="grdPersonas_SelectedIndexChanged" AutoGenerateColumns="False">
+			<Columns>
+				<asp:CommandField ShowEditButton="True" />
+				<asp:BoundField DataField="Apellido" HeaderText="Apellido" SortExpression="Apellido" />
+				<asp:BoundField DataField="Nombre" HeaderText="Nombre" SortExpression="Nombre" />
+				<asp:BoundField DataField="Direccion" HeaderText="Direccion" SortExpression="Direccion" />
+				<asp:BoundField DataField="EmailPersonal" HeaderText="EmailPersonal" SortExpression="EmailPersonal" />
+				<asp:BoundField DataField="FechaNacimiento" HeaderText="FechaNacimiento" SortExpression="FechaNacimiento" />
+				<asp:BoundField DataField="Legajo" HeaderText="Legajo" SortExpression="Legajo" />
+				<asp:BoundField DataField="Telefono" HeaderText="Telefono" SortExpression="Telefono" />
+				<asp:BoundField DataField="TiposPersona" HeaderText="TiposPersona" SortExpression="TiposPersona" />
+				<asp:BoundField DataField="ID" HeaderText="ID" SortExpression="ID" />
+				<asp:BoundField DataField="State" HeaderText="State" SortExpression="State" />
+			</Columns>
+		</asp:GridView>
+		<asp:ObjectDataSource ID="odsPersonas" runat="server" DataObjectTypeName="Business.Entities.Persona" DeleteMethod="Save" SelectMethod="GetAll" TypeName="Business.Logic.PersonaLogic" UpdateMethod="Save" InsertMethod="Save">
+		</asp:ObjectDataSource>
+        <div>
+        </div>
+	<table border="1">
+		<tr>
+			<td align="center" colspan="2">
+				<asp:Label ID="lblTablaPersona" runat="server" Text="Datos personales"></asp:Label></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Id Persona:o" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Fecha de Nacimiento:</td>
+			<td class="auto-style1">
+				<asp:TextBox ID="txtFechaNac" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Email Personal:</td>
+			<td class="auto-style1">
+				<asp:TextBox ID="txtEmailPersonal" runat="server"></asp:TextBox></td>
+		</tr>
+    </table>
+	<table border="1">
+		<tr>
+			<td align="center" colspan="2">
+				<asp:Label ID="lblDatosInstit" runat="server" Text="Datos institucionales"></asp:Label></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Legajo:</td>
+			<td class="auto-style1">
+				<asp:TextBox ID="txtLegajo" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Tipo de Persona:</td>
+			<td class="auto-style1">
+				<asp:TextBox ID="txtTipoPersona" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Plan:</td>
+			<td class="auto-style1">
+				<asp:DropDownList ID="ddlPlan" runat="server">
+				</asp:DropDownList>
+			</td>
+		</tr>
+	</table>
+	<table border="1">
+		<tr>
+			<td align="center" colspan="2">
+				<asp:Label ID="Label1" runat="server" Text="Datos para el sistema"></asp:Label></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Id:</td>
+			<td>
+				<asp:Label ID="lblIdUsuario" runat="server"></asp:Label></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Usuario:</td>
+			<td>
+				<asp:TextBox ID="txtNombreUsuario" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Clave:</td>
+                
+			<td>
+				<asp:TextBox ID="txtClave" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Confirmar Clave</td>
+			<td>
+				<asp:TextBox ID="txtConfirmaClave" runat="server"></asp:TextBox></td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Habilitado</td>
+			<td>
+
+				<asp:CheckBox ID="cbHabilitado" runat="server" />
+
+			</td>
+		</tr>
+		<tr>
+			<td style="width: 150px" align="right">
+				Email Usuario:</td>
+			<td>
+				<asp:TextBox ID="txtEmailUsuario" runat="server"></asp:TextBox></td>
+		</tr>
+        <tr>
+            <td style="width: 150px" align="center">
+                <asp:Button ID="btnGuardar" runat="server" Text="Guardar" OnClick="btnGuardar_Click" /></td>
+            <td align="center">
+	             <asp:Button ID="btnCancelar" runat="server" Text="Cancelar" /></td>
+		</tr>
+    </table>
+    </form>
+</body>
+</html>
---
+++ Web/Personas.aspx.cs
@@ -0,0 +1,159 @@
+﻿using Business.Entities;
+using Business.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class Personas : System.Web.UI.Page
+    {
+
+        UsuarioLogic ul = new UsuarioLogic();
+        PersonaLogic pl = new PersonaLogic();
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (PaginaEnEstadoEdicion())
+            {
+                int id = Int32.Parse(Request.QueryString["id"]);
+                llenaGrilla(id);
+            }
+        }
+
+        protected void grdPersonas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void grdPersonas_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.ToLower() == "insertar")
+            {
+                /*TextBox cajaTexto;
+                CheckBox cbHabil;
+                string textoActual;
+                Business.Entities.Usuario usuarioNuevo = new Business.Entities.Usuario();
+
+                //Busco el control y lo asigno a la propiedad correspondiente del objeto Usuario
+                //cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtNombre");
+                //textoActual = cajaTexto.Text;
+                //usuarioNuevo.NombreUsuario = textoActual;
+
+                //cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtApellido");
+                //textoActual = cajaTexto.Text;
+                //usuarioNuevo.Apellido = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtEmail");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Email = textoActual;
+
+                cbHabil = (CheckBox)grdUsuarios.FooterRow.FindControl("cbHabilitado");
+                usuarioNuevo.Habilitado = cbHabil.Checked;
+
+                Business.Logic.UsuarioLogic usulogic = new Business.Logic.UsuarioLogic();
+                usulogic.Save(usuarioNuevo);
+                grdUsuarios.DataBind();*/
+
+            }
+        } 
+
+        private bool PaginaEnEstadoEdicion()
+        {
+            if (Request.QueryString["id"] != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public enum ModoForm { Alta, Baja, Modificacion, Consulta };
+
+        private ModoForm _Modo;
+        public ModoForm Modo
+        {
+            get { return _Modo; }
+            set { _Modo = value; }
+        }
+
+        private void llenaGrilla(int id)
+        {
+            //Obtiene el usuario activo seleccionado
+            Business.Logic.UsuarioLogic ul = new Business.Logic.UsuarioLogic();
+            PersonaLogic pl = new PersonaLogic();
+            Business.Entities.Usuario usuarioActivo = new Business.Entities.Usuario();
+            Business.Entities.Persona personaActiva = new Business.Entities.Persona();
+
+            usuarioActivo = ul.GetOne(id);
+            personaActiva = usuarioActivo.IDPersona;
+
+            //llena la grilla con los datos del usuario traido de la bd
+
+            //Tabla de datos de persona
+            lblIdPersona.Text = personaActiva.ID.ToString();
+            txtNombre.Text = personaActiva.Nombre.ToString();
+            txtApellido.Text = personaActiva.Apellido.ToString();
+            txtDireccion.Text = personaActiva.Direccion.ToString();
+            txtTelefono.Text = personaActiva.Telefono.ToString();
+            txtFechaNac.Text = personaActiva.FechaNacimiento.ToString();
+            txtEmailPersonal.Text = personaActiva.EmailPersonal.ToString();
+
+            //Tabla de datos de usuario
+            lblIdUsuario.Text = usuarioActivo.ID.ToString();
+            txtNombreUsuario.Text = usuarioActivo.NombreUsuario;
+            cbHabilitado.Checked = usuarioActivo.Habilitado;
+            txtEmailUsuario.Text = usuarioActivo.Email;
+
+
+
+            // Hace el enlace al DataTable contenido en el DataSet
+            ddlPlan.DataSource = pl.GetAll();
+            // Hace el enlace del campo au_id para el valor
+            ddlPlan.DataValueField = "desc_plan";
+            // Llena el DropDownList con los datos de la fuente de datos
+            ddlPlan.DataBind();
+
+        }
+
+        protected void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario();
+            Persona pers = new Persona();
+            pl.GetOne(Int32.Parse(lblIdPersona.Text));
+
+            usuario.State = (Usuario.States)ModoForm.Modificacion;
+            usuario.ID = Int32.Parse(lblIdUsuario.Text);
+            usuario.NombreUsuario = txtNombreUsuario.Text;
+            usuario.Habilitado = cbHabilitado.Checked;
+            usuario.Email = txtEmailUsuario.Text;
+            usuario.Clave = txtClave.Text;
+
+            pers.ID = Int32.Parse(lblIdPersona.Text);
+            pers.State = (Persona.States)ModoForm.Modificacion;
+            pers.Nombre = txtNombre.Text;
+            pers.Apellido = txtApellido.Text;
+            pers.Direccion = txtDireccion.Text;
+            pers.Telefono = txtTelefono.Text;
+            pers.FechaNacimiento = DateTime.Parse(txtFechaNac.Text);
+            pers.EmailPersonal = txtEmailPersonal.Text;
+
+            usuario.IDPersona = pers;
+
+            ul.Save(usuario);
+
+        }
+
+        private void MapearADatos()
+        {
+            Usuario usuario = new Usuario();
+        }
+    }
+}
